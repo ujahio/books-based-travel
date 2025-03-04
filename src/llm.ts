c03d8c5acf839f20ddfbf422bf71359936e28baf
Joshua Abu <joshujah007@gmail.com>
2025-03-04 22:25:29 +0400

    add sample prompt to llm

--- src/llm.ts
+++ src/llm.ts
@@ -1,10 +1,13 @@
 import type { AIMessage } from '../types';
 import { openai } from './ai';
+import { systemPropmpt as defaultSystemPrompt } from './systemprompt';
 
-export const runLLM = async ({ userMessages }: { userMessages: AIMessage[] }) => {
+export const runLLM = async ({ userMessages, systemPrompt, temperature = 0.1, }: { userMessages: AIMessage[]; systemPrompt?: string; temperature: number }) => {
   const response = await openai.chat.completions.create({
-    model: 'gpt-4o-mini',
+    model: process.env.LLM_MODEL || 'gpt-4o-mini',
+    temperature,
     messages: [
+      { role: 'system', content: systemPrompt || defaultSystemPrompt },
       ...userMessages
     ],
   });
---
+++ src/systemprompt.ts
@@ -0,0 +1,11 @@
+export const systemPropmpt = `
+  You are a a helpful assistant that can recommend a travel destination based on the book selection and also recommend a cuisine from travel destination and book selection.
+
+  - Do not give a summary or explanation of the book, travel destination or cuisine. Just simply list the location and cuisine in plain terms.
+  - If you don't have a recommendation, that ok. Just say so.
+  - You may not get the author name, but a book must be selected from the user
+
+  Example output:
+  - Travel Destination: Egypt
+  - Food Recommendation: Koshari, Baklava, Ta'ameya and Ful Medames
+`
