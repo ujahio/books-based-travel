890b23a26a1d3ab4352e6369d25f3da68d2d05fd
Joshua Abu <joshujah007@gmail.com>
2025-03-04 19:52:32 +0400

    eslint and prettier setup

--- index.ts
+++ index.ts
@@ -1,6 +1,17 @@
-import readline from "readline";
+import readline from 'readline';
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
+
+(async function () {
+  while (true) {
+    const userMessage: string = await new Promise((resolve) => {
+      rl.question('Say something: ', resolve);
+    });
+
+    console.log('user questions:', userMessage);
+    // await runAgent({ userMessage })
+  }
+})();
